7d8ddb700947a8bd4dec42f4eb4f93d631f86688
Jacob Walter <walte250@purdue.edu>
2025-01-05 17:40:23 -0500

    TRIANGLE!

--- include/globals.h
+++ include/globals.h
@@ -1,10 +1,19 @@
 #ifndef GLOBALS_H
 #define GLOBALS_H
+#include "glad.h"
+#include "SDL.h"
+
+#include <string>
 
 extern int             gScreenHeight;
 extern int             gScreenWidth;
 extern SDL_Window*     gGraphicsWindow;
 extern SDL_GLContext   gOpenGLContext;
 extern bool            gQuit;
+extern GLuint          gVertexArrayObject;
+extern GLuint          gVertexBufferObject;
+extern GLuint          gGraphicsPipelineShaderProgram;
 
+extern std::string     gVertexShaderSource;
+extern std::string     gFragmentShaderSource;
 #endif
